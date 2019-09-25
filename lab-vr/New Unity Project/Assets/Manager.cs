b155cff9e1017882b10f08975b2aa1113133a146
bcruz94 <bcruz94@gmail.com>
2019-09-25 17:55:26 -0400

    Completed lab 1 vr

---
+++ lab-vr/New Unity Project/Assets/Manager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public bool hayBaleReturned = false;
+
+
+    public GameObject finishText;
+
+    public void checkForVictory()
+    {
+        if (hayBaleReturned)
+        {
+            finishText.SetActive(true);
+        }
+    }
+
+    public void hayBaleWasReturned()
+    {
+        this.hayBaleReturned = true;
+        checkForVictory();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
